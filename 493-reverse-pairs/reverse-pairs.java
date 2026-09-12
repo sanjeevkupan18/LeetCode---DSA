07e8a5ab49424dd82650ce85f1e5fc36593c7df7
Sanjeev Kumar Pandit <sanjeevkupan18@gmail.com>
2026-09-12 23:11:48 +0530

    Time: 43 ms (72.07%) | Memory: 64.5 MB (31.39%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.java
@@ -0,0 +1,79 @@
+class Solution {
+
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    private int mergeSort(int[] nums, int left, int right) {
+
+        if (left >= right) {
+            return 0;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        int count = 0;
+
+        // Count reverse pairs in left half
+        count += mergeSort(nums, left, mid);
+
+        // Count reverse pairs in right half
+        count += mergeSort(nums, mid + 1, right);
+
+        // Count reverse pairs across both halves
+        count += countReversePairs(nums, left, mid, right);
+
+        // Merge the two sorted halves
+        merge(nums, left, mid, right);
+
+        return count;
+    }
+
+    private int countReversePairs(int[] nums, int left, int mid, int right) {
+
+        int j = mid + 1;
+        int count = 0;
+
+        for (int i = left; i <= mid; i++) {
+
+            while (j <= right &&
+                   (long) nums[i] > 2L * nums[j]) {
+                j++;
+            }
+
+            count += j - (mid + 1);
+        }
+
+        return count;
+    }
+
+    private void merge(int[] nums, int left, int mid, int right) {
+
+        int[] temp = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = nums[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = nums[j++];
+        }
+
+        for (int x = 0; x < temp.length; x++) {
+            nums[left + x] = temp[x];
+        }
+    }
+}
